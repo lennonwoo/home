6d7af7a7c38ba11ea25d1670ba44e140a9fccc9e
lennonwoo <cccmorin@gmail.com>
2018-08-07 20:59:14 +0800

    generate wav for speed

--- home/src/config.py
+++ home/src/config.py
@@ -1,64 +1,141 @@
 # -*- coding: utf-8 -*-
 import dynamic_reconfigure.client
 
-from job import Job, JobTest
+from asr import AsrJobComplicative, AsrJobNameObj, AsrConfirm
 
 
 class Config:
     distance = {
-        'people': 0.5,
+        'people': 1.1,
         'face': 0.5,
 
-        'wanglaoji': 0.5,
-        'fenda': 0.5,
-        'kele': 0.5,
-        'xuebi': 0.5,
-        'shui': 0.5,
-        'hongcha': 0.5,
-        'shupian': 0.5,
+        'wanglaoji': 0.7,
+        'fenda': 0.7,
+        'kele': 0.7,
+        'xuebi': 0.7,
+        'shui': 0.7,
+        'hongcha': 0.7,
+        'shupian': 0.7,
     }
+
     box_threshold = {
-        'people': 0.7,
+        'people': 0.6,
         'face': 0.9,
 
-        'wanglaoji': 0.8,
-        'fenda': 0.8,
-        'kele': 0.8,
-        'xuebi': 0.8,
-        'shui': 0.8,
-        'hongcha': 0.8,
-        'shupian': 0.8,
+        'wanglaoji': 0.6,
+        'fenda': 0.6,
+        'kele': 0.6,
+        'xuebi': 0.6,
+        'shui': 0.6,
+        'hongcha': 0.6,
+        'shupian': 0.6,
     }
+
+    names_cn = [
+        "拉文",
+        "丹尼尔",
+        "迈克尔",
+        "杰克",
+        "费希尔",
+        "凯文",
+        "露丝",
+        "约翰",
+        "玛丽",
+        "亚当",
+    ]
+
+    objs_cn = [
+        "芬达",
+        "薯片",
+        "可乐",
+        "冰红茶",
+        "矿泉水",
+        "雪碧",
+        "王老吉",
+    ]
+
+    objs_en = [
+        "fenda",
+        "shupian",
+        "kele",
+        "hongcha",
+        "shui",
+        "xuebi",
+        "wanglaoji",
+    ]
+
+    @staticmethod
+    def get_obj_pose():
+        dic = {}
+        for obj in Config.objs_en:
+            dic[obj] = None
+        return dic
+
+    @staticmethod
+    def get_obj_en2cn():
+        dic = {}
+        for obj_en, obj_cn in zip(Config.objs_en, Config.objs_cn):
+            dic[obj_en] = obj_cn
+        return dic
+
+    @staticmethod
+    def get_obj_cn2en():
+        dic = {}
+        for obj_en, obj_cn in zip(Config.objs_en, Config.objs_cn):
+            dic[obj_cn] = obj_en
+        return dic
 
     poses_file_path = "/home/lennon/home_ws/.cache/poses_20180801.xml"
 
-    play_command = "aplay"
-    self_intro_wav = "/home/lennon/Desktop/self_intro.wav"
-    body_down_wav = "/home/lennon/Desktop/body_down.wav"
-    hello_name_job_wav = "/home/lennon/Desktop/hello_name_job.wav"
-    next_guest_wav = "/home/lennon/Desktop/next_guest.wav"
-    again_wav = "/home/lennon/Desktop/again.wav"
-
-    people_num = 4
+    # meet guest part
+    people_num = 1
+    find_people_retry = False
 
     speak_pub_topic = "/xf/tts/words"
-    eye_sub_topic = "/camera/color/image_raw"
-
     asr_action_topic = "/xf_asr/home_recognize"
     tts_action_topic = "/xf_tts/tts_generate"
     yolo_action_topic = "/darknet_ros/check_for_objects"
 
-    debug = True
+    debug = False
 
-    # job_class = Job
-    job_class = JobTest
+    asr_job_class = AsrJobNameObj
+    asr_confirm_class = AsrConfirm
+    asr_continue_time = 6
 
-    save_base_path = "/home/lennon/Desktop/"
+    # wav part
+    play_command = "play"
+    wav_speed_up = "1.2"  # Notes: use str
 
-    asr_continue_time = 8
+    base_path = "/home/lennon/Desktop/"
+    wav_base_path = base_path + "wav/"
 
+    self_intro_wav     = wav_base_path + "self_intro.wav"
+    body_down_wav      = wav_base_path + "body_down.wav"
+    hello_name_job_wav = wav_base_path + "hello_name_job.wav"
+    next_guest_wav     = wav_base_path + "next_guest.wav"
+    again_wav          = wav_base_path + "again.wav"
+    then_again_wav     = wav_base_path + "then_again.wav"
+    stranger_wav       = wav_base_path + "stranger.wav"
+
+    broadcast_job_wav_base_path = wav_base_path + "broadcast_job/"
+    broadcast_job_wav_msg_format = "你是%s，我要拿%s"
+    broadcast_job_wav_path_format = broadcast_job_wav_base_path + broadcast_job_wav_msg_format +  ".wav"
+
+    confirm_job_wav_base_path = wav_base_path + "confirm_job/"
+    confirm_job_wav_msg_format = "你是%s，我要拿%s，是吗"
+    confirm_job_wav_path_format = confirm_job_wav_base_path + confirm_job_wav_msg_format + ".wav"
+
+    hello_job_wav_base_path = wav_base_path + "confirm_job/"
+    hello_job_wav_msg_format = "你好%s，你的%s"
+    hello_job_wav_path_format = hello_job_wav_base_path + hello_job_wav_msg_format + ".wav"
+
+    obj_wav_base_path = wav_base_path + "obj/"
+    obj_wav_msg_format = "%s"
+    obj_wav_path_format = obj_wav_base_path + obj_wav_msg_format + ".wav"
+
+    # dynamic costmap param change part
     @staticmethod
-    def change_inflation(robot_radius=0.18, inflation_radius=0.3):
+    def change_costmap_params(robot_radius=0.18, inflation_radius=0.3):
         client = dynamic_reconfigure.client.Client("move_base/global_costmap", timeout=10)
         client.update_configuration({"robot_radius": robot_radius})
         client = dynamic_reconfigure.client.Client("move_base/global_costmap/inflation_layer", timeout=10)
@@ -67,3 +144,17 @@ class Config:
         client.update_configuration({"robot_radius": robot_radius})
         client = dynamic_reconfigure.client.Client("move_base/local_costmap/inflation_layer", timeout=10)
         client.update_configuration({"inflation_radius": inflation_radius})
+
+    @staticmethod
+    def decrease_costmap():
+        Config.change_costmap_params(0.1, 0.1)
+        pass
+
+    @staticmethod
+    def increase_costmap():
+        Config.change_costmap_params(0.18, 0.3)
+        pass
+
+    # arm part
+    arm_port_name = '/dev/ttyUSB0'
+    arm_baud = 57600
---
+++ home/src/generate_wav.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import os
+
+import rospy
+import actionlib
+
+from std_msgs.msg import String
+from xf_ros.msg import *
+
+from config import Config
+
+
+class GenerateWav:
+    def __init__(self, generator=None):
+        self.tts_action = actionlib.SimpleActionClient("/xf_tts/tts_generate", TTSAction)
+        self.tts_action.wait_for_server(rospy.Duration(3))
+        self.generator = generator
+
+    def generate_wav(self):
+        for msg, path in self.generator:
+            audio_path = self._generate_wav_via_tts(msg)
+            print(msg, audio_path)
+
+            cmd = " ".join(['mv', audio_path, path])
+            print(msg, cmd)
+            os.system(cmd)
+
+    def change_format(self, generator):
+        self.generator = generator
+
+    def _generate_wav_via_tts(self, s):
+        goal = TTSGoal()
+        msg = String()
+        msg.data = s
+        goal.tts_str = msg
+
+        self.tts_action.send_goal(goal)
+        self.tts_action.wait_for_result(rospy.Duration(20))
+
+        audio_path = self.tts_action.get_result().audio_path.data
+        return audio_path
+
+
+class BasicGenerator:
+    def __init__(self, msg_format, path_format):
+        self.msg_format = msg_format
+        self.path_format = path_format
+
+
+class NameObjMsgPathGenerator(BasicGenerator):
+    def __init__(self, msg_format, path_format):
+        BasicGenerator.__init__(self, msg_format, path_format)
+
+    def __iter__(self):
+        # based on home's 10 people names and 10 obj names
+        for name in Config.names_cn:
+            for obj in Config.objs_cn:
+                msg = self.msg_format % (name, obj)
+                path = self.path_format % (name, obj)
+                yield msg, path
+
+
+class ObjMsgPathGenerator(BasicGenerator):
+    def __init__(self, msg_format, path_format):
+        BasicGenerator.__init__(self, msg_format, path_format)
+
+    def __iter__(self):
+        for obj in Config.objs_cn:
+            msg = self.msg_format % obj
+            path = self.path_format % obj
+            yield msg, path
+
+
+def main():
+    os.system("""tmux new-window -c ~/home_ws "source devel/setup.zsh && roslaunch home xf.launch" """)
+    rospy.init_node('generate_name_obj_wav')
+
+    gw = GenerateWav()
+
+    gw.change_format(NameObjMsgPathGenerator(Config.broadcast_job_wav_msg_format,
+                                             Config.broadcast_job_wav_path_format))
+    gw.generate_wav()
+
+    gw.change_format(NameObjMsgPathGenerator(Config.confirm_job_wav_msg_format,
+                                             Config.confirm_job_wav_path_format))
+    gw.generate_wav()
+
+    gw.change_format(ObjMsgPathGenerator(Config.obj_wav_msg_format,
+                                         Config.obj_wav_path_format))
+    gw.generate_wav()
+
+
+if __name__ == '__main__':
+    main()
