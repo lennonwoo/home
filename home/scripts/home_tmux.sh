2da48b408cb3eb126f3184bce6f79d47c9f0ff9c
lennonwoo <cccmorin@gmail.com>
2018-08-07 09:27:28 +0800

    fix startup scripts ctrl+c exit then pane removed situation

--- home/scripts/home_tmux.sh
+++ home/scripts/home_tmux.sh
@@ -1,9 +1,14 @@
 # 创建名为my-session的会话
-tmux new-window -c ~/home_ws/ 'source devel/setup.zsh && roslaunch home obj_detect.launch'
-sleep 3
-tmux split-window -h -c ~/home_ws/ 'source devel/setup.zsh && roslaunch home navigation.launch'
-sleep 2
-tmux split-window -v -c ~/home_ws/ 'source devel/setup.zsh && roslaunch home xf.launch'
-sleep 2
+tmux new-window -c
+tmux split-window -h
+tmux split-window -v
 tmux select-pane -L
-tmux split-window -v -c ~/home_ws/ 'source devel/setup.zsh && roslaunch home home_sm.launch'
+tmux split-window -v
+
+tmux send-keys -t 0.1 'cd ~/home_ws && source devel/setup.zsh && roslaunch home obj_detect.launch' Enter
+sleep 3
+tmux send-keys -t 0.2 'cd ~/home_ws && source devel/setup.zsh && roslaunch home navigation.launch' Enter
+sleep 2
+tmux send-keys -t 0.3 'cd ~/home_ws && source devel/setup.zsh && roslaunch home xf.launch' Enter
+sleep 2
+tmux send-keys -t 0.4 'cd ~/home_ws && source devel/setup.zsh && roslaunch home home_sm.launch' Enter
