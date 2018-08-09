29a1e78ebe3868ef25db36c65e27595297554a8c
lennonwoo <cccmorin@gmail.com>
2018-08-09 19:07:59 +0800

    robot arm!

--- home/src/robot/body.py
+++ home/src/robot/body.py
@@ -44,9 +44,60 @@ class Arm(RobotPart):
 
         port = self.config.arm_port_name
         baud = self.config.arm_baud
-        self.ser = serial.Serial(port, baud)
 
-    def grasp(self, x=0, y=0):
-        self.ser.open()
-        self.ser.write("test")
-        self.ser.read("test")
+        if self.config.enable_arm:
+            self.ser = serial.Serial(port, baud)
+            self.ser.bytesize = serial.EIGHTBITS
+            self.ser.timeout = 0.5
+
+            print("connect arm")
+            self.connect_arm()
+
+    def connect_arm(self):
+        self.ser.write(self.config.ARM_CONNECT_COMMAND)
+        self.wait_until_arm_ready()
+
+    def wait_until_arm_ready(self):
+        sleep_count = 0
+        while sleep_count < 20:
+            print("sleep count: ", sleep_count)
+            content_read = self.ser.read(2)
+            if content_read == "ok":
+                return True
+            else:
+                sleep_count += 1
+
+        print("wait arm ready time out, you need reset arm")
+        return False
+
+    def reset_arm(self):
+        self.ser.write(self.config.ARM_RESET_COMMAND)
+        print("wait reset_ok")
+        return self.wait_until_arm_ready()
+
+    def close_obj(self):
+        self.ser.write(self.config.ARM_CLOSE_OBJ_COMMAND)
+        print("wait close_ok")
+        return self.wait_until_arm_ready()
+
+    def grasp_obj(self):
+        self.ser.write(self.config.ARM_GRASP_OBJ_COMMAND)
+        print("wait grasp_ok")
+        return self.wait_until_arm_ready()
+
+    def take_obj(self):
+        self.ser.write(self.config.ARM_TAKE_OBJ_COMMAND)
+        print("wait take_back_ok")
+        return self.wait_until_arm_ready()
+
+    def grasp(self, obj_name="kele"):
+        print("close arm")
+        if not self.close_obj():
+            print("I will reset arm for not close obj")
+            return self.reset_arm()
+
+        if obj_name in self.config.arm_grasp_obj_list:
+            self.grasp_obj()
+            self.take_obj()
+        else:
+            self.reset_arm()
